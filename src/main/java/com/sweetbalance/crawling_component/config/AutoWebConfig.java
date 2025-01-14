f366336d5ca0b7b268da756076d0e196c3642924
kisusu <kikisu115@naver.com>
2025-01-14 22:33:59 +0900

    feat: init project

---
+++ src/main/java/com/sweetbalance/crawling_component/config/AutoWebConfig.java
@@ -0,0 +1,10 @@
+package com.sweetbalance.crawling_component.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AutoWebConfig {
+
+}
---
+++ src/test/java/com/sweetbalance/crawling_component/CrawlingComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweetbalance.crawling_component;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlingComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
