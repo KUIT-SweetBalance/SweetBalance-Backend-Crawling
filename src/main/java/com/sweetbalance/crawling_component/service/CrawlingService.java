2fa0c001d3168a2cb745603d08881c33464c4411
kisusu <kikisu115@naver.com>
2025-01-15 15:07:22 +0900

    feat: 음료 중복 체크 시 이름과 브랜드를 모두 고려하도록 로직 변경

--- src/main/java/com/sweetbalance/crawling_component/service/CrawlingService.java
+++ src/main/java/com/sweetbalance/crawling_component/service/CrawlingService.java
@@ -2,6 +2,7 @@ package com.sweetbalance.crawling_component.service;
 
 import com.sweetbalance.crawling_component.crawler.StarbucksCrawler;
 import com.sweetbalance.crawling_component.entity.Beverage;
+import com.sweetbalance.crawling_component.entity.BeverageSize;
 import com.sweetbalance.crawling_component.repository.BeverageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +27,8 @@ public class CrawlingService {
         List<Beverage> starbucksData = starbucksCrawler.crawlBeverageList();
         for (Beverage newBeverage : starbucksData) {
 
-            Optional<Beverage> existingBeverage = beverageRepository.findByName(newBeverage.getName());
+            Optional<Beverage> existingBeverage =
+                    beverageRepository.findByNameAndBrand(newBeverage.getName(), newBeverage.getBrand());
 
             if (existingBeverage.isPresent()) {
                 Beverage beverage = existingBeverage.get();
@@ -47,13 +49,28 @@ public class CrawlingService {
         // 이름과 브랜드는 변경되지 않는다고 가정
         // existingBeverage.setName(newBeverage.getName());
         // existingBeverage.setBrand(newBeverage.getBrand());
-        // existingBeverage.setBrand(newBeverage.getImgUrl());
 
         existingBeverage.setCategory(newBeverage.getCategory());
+        existingBeverage.setImgUrl(newBeverage.getImgUrl());
+
+        // 기존 BeverageSize 업데이트 로직 - 새로운 사이즈 추가
+        updateBeverageSizes(existingBeverage, newBeverage.getSizes());
+
         existingBeverage.setSugar(newBeverage.getSugar());
         existingBeverage.setCalories(newBeverage.getCalories());
         existingBeverage.setCaffeine(newBeverage.getCaffeine());
+
         existingBeverage.setStatus(newBeverage.getStatus());
+    }
+
+    private void updateBeverageSizes(Beverage existingBeverage, List<BeverageSize> newSizes) {
+        for (BeverageSize newSize : newSizes) {
+            boolean exists = existingBeverage.getSizes().stream()
+                    .anyMatch(size -> size.getSizeType().equals(newSize.getSizeType()) && size.getVolume() == newSize.getVolume());
+            if (!exists) {
+                existingBeverage.addSize(newSize);
+            }
+        }
     }
 }
 
